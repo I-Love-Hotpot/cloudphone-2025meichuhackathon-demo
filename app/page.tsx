a00cb1fe5d67e2cb0e4cab71e949ee72c4a7998f
Sz-Wei Chen <swchen1217@gmail.com>
2026-09-18 18:27:40 +0800

    test: 02

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="max-w-xs mx-auto p-2 min-h-screen flex flex-col justify-center items-center text-center">
       <main className="flex-1 flex flex-col justify-center items-center space-y-4">
-        <h1 className="text-lg font-bold text-gray-600">Welcome!AAA</h1>
+        <h1 className="text-lg font-bold text-gray-600">Welcome!AAA222</h1>
         <p className="text-sm text-gray-500 leading-tight">
           It&apos;s a demo page.
         </p>
